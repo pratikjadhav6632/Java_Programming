11cf32552698633251e39144a118759d702db0c3
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-05 22:55:24 +0530

    Learn about primitive data types
    Signed-off-by: Pratik Jadhav <pratikjadhav6632@gmail.com>

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int age=20;
+        var name="Pratik";
+        System.out.println(name);
+        System.out.println(age);
+
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+    }
+}
