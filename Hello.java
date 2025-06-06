a76615b7ecee35c94ad14e5278185b32f8692837
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-06 18:08:26 +0530

    check length of data types

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{
     public static void main(String[] args){
         System.out.println("Hello World");
+
         int age=20;
         var name="Pratik";
         System.out.println(name);
@@ -8,5 +9,6 @@ class Hello{
 
 //        System.out.println(Integer.MIN_VALUE);
 //        System.out.println(Integer.MAX_VALUE);
+
     }
 }
