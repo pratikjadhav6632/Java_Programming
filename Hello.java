2aa1ccf1f55898fde3936de03b21e48db894093a
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-08 23:11:15 +0530

    Update marks variable to use an integer value and add output for count

--- Hello.java
+++ Hello.java
@@ -4,10 +4,13 @@ class Hello{
 
         int age=20;
         var name="Pratik";
-        float marks=98.12;
+        float marks=98;
         System.out.println(marks);
         System.out.println(name);
         System.out.println(age);
+
+        double count=12.32;
+        System.out.println(count);
 
 //        System.out.println(Integer.MIN_VALUE);
 //        System.out.println(Integer.MAX_VALUE);
