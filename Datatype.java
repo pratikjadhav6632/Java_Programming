b514294b6e77d9a3604660131483a33dc0c2b7ed
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-12 22:32:06 +0530

    Learn about datatypes

---
+++ Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype {
+    public static void main(String[]args){
+        byte num=1;
+        short data=213;
+        int salary=12000;
+        long numbers=12343567l;
+        float ratio=3.14f;
+        double marks=99.99;
+        boolean isAllow=true | false;
+        char letter='A';
+    }
+}
