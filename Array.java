7f68effad995c4bb98190aea377a13d46d9e123b
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-21 01:29:20 +0530

    solve array based questions

--- Array.java
+++ Array.java
@@ -43,5 +43,43 @@ public class Array {
             System.out.println();
         }
 
+        // Questions
+
+        // Search an element in array
+
+        int[] Sarr = { 1, 4, 5, 67, 5 };
+        for (int num : Sarr) {
+            if (num == 67) {
+                System.out.println("Searched num is Found");
+            }
+        }
+
+        // Find Max number
+
+        int[] Marr = { 3, 4, 54, 3453, 5663, -2 };
+        int Mres = Integer.MIN_VALUE;
+        for (int i : Marr) {
+            if (i > Mres) {
+                Mres = i;
+            }
+        }
+        System.out.println("Max num is : " + Mres);
+
+        // Reverse Array
+
+        int[] Rarr = { 1, 2, 34, 5, 67 };
+        for (int i = Rarr.length - 1; i >= 0; i--) {
+            System.out.print(Rarr[i] + " | ");
+        }
+        System.out.println();
+        // sum of array
+
+        int[] Sum_arr = { 1, 4, 7, 3, 8 };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = Sum_arr[i] + sum;
+        }
+        System.out.println("Sum of array is : " + sum);
+
     }
 }
