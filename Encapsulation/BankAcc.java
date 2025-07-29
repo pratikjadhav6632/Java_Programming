1603a95bb5042d1f11b26b732b4cde73c8a8ce66
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-07-29 10:28:01 +0530

    learn encapsulation

---
+++ Encapsulation/BankAcc.java
@@ -0,0 +1,21 @@
+package Encapsulation;
+public class BankAcc{
+    private double  balance;
+
+    public double  getBalance(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if(amount >0){
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount){
+        if(amount >0 && amount <= balance){
+            balance -= amount;
+        }
+    }
+}
+
