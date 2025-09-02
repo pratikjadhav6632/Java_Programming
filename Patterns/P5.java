0593f2956bdcc0fcbb9e12a45ea9fb5c5e8ceafc
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-09-03 01:03:03 +0530

    P5-Invereted Half pyramid (180 deg)

---
+++ Patterns/P5.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.*;
+public class P5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=4;
+        System.out.println("Enter value of n:");
+        n=sc.nextInt();
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("   ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
