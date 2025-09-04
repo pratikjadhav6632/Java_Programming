d3d3578ae8baeaf465315c46d9be95188178f510
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-09-04 23:21:00 +0530

    P12-Solid rohmbus

--- Patterns/prac.java
+++ Patterns/prac.java
@@ -59,5 +59,31 @@ public class prac {
             }
             System.out.println();
         }
+
+        //0-1 triangle
+        System.out.println("0-1 triangle");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print(" 1 ");
+                }else{
+                    System.out.print(" 0 ");
+                }
+            }
+            System.out.println();
+        }
+
+        //floyds half pyramid 
+        System.out.println("Floids alf pyramid");
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+
     }
 }
