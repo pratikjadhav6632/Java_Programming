0b32d60345d2f4a43a3089cd7a333c0b5f6b524c
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-09-20 22:39:56 +0530

    recursion

--- Recursion/Rec.java
+++ Recursion/Rec.java
@@ -41,6 +41,47 @@ public class Rec {
 
     }
 
+    //print fibonnaci series
+    public static void printFab(int a,int b,int k){
+        if(k==0){
+            return;
+        }
+        int c=a+b;
+        System.out.println(c);
+        printFab(b,c, k-1);
+
+    }
+
+    //print x^p
+    public static int printXResP(int x,int p){
+        if(p==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        int xPowp=printXResP(x, p-1);
+        int res=x*xPowp;
+        return res;
+    }
+
+    //print xPown (stack height logn)
+    public static int calcX(int q,int w){
+
+        if(q==0){
+            return 0;
+        }
+        if(w==0){
+            return 1;
+        }
+        //if w is even
+        if(w%2==0){
+            return calcX(q, w/2)*calcX(q, w/2);
+        }else{
+            return calcX(q, w/2)*calcX(q, w/2)*q;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int m = 1;
@@ -49,10 +90,38 @@ public class Rec {
         // print num 1 to n
         printNumI(m);
         // calculate sum of n natural nums;
+        System.out.println("Sum of natural num");
         prinSum(1, 5, 0);
         //print fact
+        System.out.println("Factorial");
         int fact=printFact(5);
         System.out.println(fact);
+
+        //print fibbonaci series
+        System.out.println("Fibonacci series");
+        int a=0,b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int k=7;
+        printFab(a, b, k-2);
+
+        //print x^n (stack height)
+        System.out.println("Print x^p");
+        int x=2;
+        int p=5;
+        int ans=printXResP(x, p);
+        System.out.println(ans);
+        
+
+        
+        //print x^n (stack height logn)
+        System.out.println("Print x^p (logn)");
+        int q=2;
+        int w=5;
+        int ans2=calcX(q, w);
+        System.out.println(ans2);
+
+
     }
 
 }
